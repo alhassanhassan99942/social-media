a656737cafb5f1f2f82a12eac2fe315a4691b290
alhassanhassan99942 <alhassanhassan99942@gmail.com>
2025-03-13 16:21:31 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Button } from 'react-native'
+import { useRouter } from 'expo-router'
+import Welcome from './welcome'
+import ScreenWrapper from '../components/ScreenWrapper'
+
+const index = () => {
+    const router = useRouter()
+  return (
+    <ScreenWrapper>
+      <Text>index</Text>
+      <Button title='welcome' onPress={() => router.push('welcome')}/>
+    </ScreenWrapper>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
---
+++ constants/theme.js
@@ -0,0 +1,27 @@
+export const theme = {
+    colors: {
+        primary: '#00C26F',
+        primaryDark: '#00AC62',
+        dark: '#3E3E3E',
+        darlLight: '#E1E1E1',
+        gray: '#e3e3e3',
+
+        text: '#494949',
+        textLight: '#7C7C7C',
+        textDark: '#f87171',
+    }, 
+    font: { 
+        meduim: '500',
+        meduimsemibold: '600',
+        bold: '700',
+        extrabold: '800',
+    },
+    raduis: {
+        xs: 10,
+        sm: 12,
+        md: 14,
+        lg: 16,
+        xl: 18,
+        xxl: 22,
+    }
+}
